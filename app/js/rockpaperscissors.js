cb08470ddc6ad6390f7d9037604d12f621b6066a
Eunice Park <parkeunicem@gmail.com>
2015-03-09 14:44:44 -0700

    UNIT 5 playToFive()

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -35,7 +35,6 @@ function getComputerMove(move) {
     return move || randomPlay();
 }
 
-
 function getWinner(playerMove,computerMove) {
     var winner;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
@@ -73,8 +72,25 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+    var playerMove;
+    var computerMove;
+    var winner;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
+    while (playerWins < 5 && computerWins < 5) {
+        playerMove = getPlayerMove(null);
+        computerMove = getComputerMove(null);
+        winner = getWinner(playerMove,computerMove) ;
+        if (winner === 'player') {
+            playerWins += 1;
+        } else if (winner === 'computer') {
+            computerWins += 1;
+        }
+        console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
+        console.log("The score is currently " + playerWins + " to " + computerWins + "\n");
+    }
+    console.log("Winner is " + (playerWins>computerWins? 'Player':'Computer'));
     return [playerWins, computerWins];
 }
 
+playToFive();
